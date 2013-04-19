dec7a23d2ca4e8d56d984f548e79fd1e2e13cd07
suzak <suza9g@gmail.com>
2013-04-19 10:05:43 +0000

    handle SIGINT

--- trellobot.js
+++ trellobot.js
@@ -6,3 +6,5 @@ var TrelloBot = require('./lib/trello/ircbot.js');
 var config = require('./lib/config.js').load(process.env['CONFIG_FILE']);
 
 var bot = new TrelloBot(config);
+
+process.on('SIGINT', bot.disconnect.bind(bot));

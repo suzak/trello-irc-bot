2f73de335f5b1f2c10c84bdb18244afefe64d2c9
suzak <suza9g@gmail.com>
2013-04-22 01:07:07 +0000

    disconnect

--- trellobot.js
+++ trellobot.js
@@ -7,4 +7,7 @@ var config = require('./lib/config.js').load(process.env['CONFIG_FILE']);
 
 var bot = new TrelloBot(config);
 
-process.on('SIGINT', bot.disconnect.bind(bot));
+process.on('SIGINT', function () {
+    bot.disconnect();
+    process.exit(0);
+});
